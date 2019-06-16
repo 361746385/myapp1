1156934e6a4eb89af37ffac6a21953eb62c42a33
deyi <361746385@qq.com>
2019-06-16 11:59:49 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapp1/ssclass/ssclass1.java
@@ -0,0 +1,12 @@
+package com.example.myapp1.ssclass;
+
+import android.util.Log;
+
+public class ssclass1 {
+    public int create() {
+        for (int index = 0; index < 10; index++) {
+            Log.i("ssdebugClass1", "index" + index);
+        }
+        return 0;
+    }
+}
